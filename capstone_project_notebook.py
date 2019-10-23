1f9168f97a3342ddf084e450a14ecb11ab949a6d
svetlanafrenkel <sveta@frenkel-online.com>
2019-10-23 15:07:41 -0500

    add first notebook to repository

---
+++ capstone_project_notebook.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# This notebook will be mainly used for the IBM Data Science certificate capstone project
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
